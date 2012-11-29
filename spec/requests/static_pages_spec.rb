4d5478de3119ba4fe3994ca469a4ca9b18c3a250
Bo Liang <rbaggio018@gmail.com>
2012-11-28 19:32:54 -0500

    Add titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,46 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  describe "Home Page" do
+
+    it "should have the h1 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector("h1", :text => "Home")
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector("title", :text => "Home")
+    end
+  end
+
+  describe "Help Page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector("h1", :text => "Help")
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector("title", :text => "Help")
+    end
+  end
+
+  describe "About Page" do
+
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector("h1", :text => "About Us")
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector("title", :text => "About Us")
+    end
+  end
+
   # describe "GET /static_pages" do
   #   it "works! (now write some real specs)" do
   #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -8,27 +48,4 @@ describe "StaticPages" do
   #     response.status.should be(200)
   #   end
   # end
-  describe "Home Page" do
-
-    it "should have the content 'Home'" do
-      visit '/static_pages/home'
-      page.should have_content("Home")
-    end
-  end
-
-  describe "Help Page" do
-
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content("Help")
-    end
-  end
-
-  describe "About Page" do
-
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_content("About Us")
-    end
-  end
 end
