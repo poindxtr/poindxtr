b3005e0b2115206227ce70821c5893c574c21ef0
Bo Liang <rbaggio018@gmail.com>
2012-12-04 14:47:11 -0500

    Added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,13 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  before_save { |user| user.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
 end
